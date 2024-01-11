40ea397baea2e7b5783403862e7a082eadcbe4a1
anaborgestri <aborges318@gmail.com>
2024-01-11 15:34:59 +0100

    Adjust styling for °C & add Java script for search city and change temp

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let cityTemp = response.data.temperature.current;
+  let cityTempdegrees = document.querySelector("#temp-degrees");
+  cityTempdegrees.innerHTML = Math.round(cityTemp);
+}
+function giveWeather(city) {
+  let apiKey = `4c08634eb8b52t7acf769o96f5812f64`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function findCity(event) {
   event.preventDefault();
   let input = document.querySelector("#form-search-city");
-  let city = document.querySelector(".city");
-  city.innerHTML = input.value;
+  let cityName = document.querySelector(".city");
+  cityName.innerHTML = input.value;
+  giveWeather(input.value);
 }
 
 let searchCityform = document.querySelector("#search-form");
