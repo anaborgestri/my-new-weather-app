64dbfe9674eb790dc11b8a89a6f5c556ab8d54b9
anaborgestri <aborges318@gmail.com>
2024-01-11 15:45:52 +0100

    add js for city and temp search

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function refreshWeather(response) {
   let cityTemp = response.data.temperature.current;
   let cityTempdegrees = document.querySelector("#temp-degrees");
   cityTempdegrees.innerHTML = Math.round(cityTemp);
+  //to make sure that the city name always shows up correctly instead of small or capital letters entered by the users
+  //the two elements from funtion 'findCity' need to be changed - in this case for tracebility they were just deactivated with the //
+  //they are copied to this funtion and then the 'cityName element on the second request is changed as shown below
+  //basically it is changed to get the info from the API, which is always correct (input.value changed to response.data.city;)
+  let cityName = document.querySelector(".city");
+  cityName.innerHTML = response.data.city;
+  //this way independently how the city name is written by the user it will always show up correctly
 }
 function giveWeather(city) {
   let apiKey = `4c08634eb8b52t7acf769o96f5812f64`;
@@ -13,10 +20,13 @@ function giveWeather(city) {
 function findCity(event) {
   event.preventDefault();
   let input = document.querySelector("#form-search-city");
-  let cityName = document.querySelector(".city");
-  cityName.innerHTML = input.value;
+  //let cityName = document.querySelector(".city");
+  // cityName.innerHTML = input.value;
   giveWeather(input.value);
 }
 
 let searchCityform = document.querySelector("#search-form");
 searchCityform.addEventListener("submit", findCity);
+
+//by the functionality added below we do not need to have the city and weather details in the html. everytime that is refreashed it will just show the details from the city that is below in brackets
+giveWeather("Basel");
